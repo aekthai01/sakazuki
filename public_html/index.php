4e4e1e1b4239c93360695dc45742ce5f690071ef
aekthai01 <aekgamer04@gmail.com>
2026-09-15 00:07:41 +0700

    Add root index redirect

---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'includes/auth.php';
+
+// Redirect logged in users to their dashboard
+if (isLoggedIn()) {
+    redirectByRole();
+}
+
+// Redirect to login
+header('Location: login.php');
+exit();
+?>

74c6e29b2a87ca4b9e51f801dbe979d2bcd03713
aekthai01 <aekgamer04@gmail.com>
2026-09-15 00:12:57 +0700

    Add admin discount endpoint

---
+++ public_html/admin/get_discount.php
@@ -0,0 +1,6 @@
+<?php
+require_once '../includes/auth.php';
+requireAdmin();
+
+header('Content-Type: application/json');
+echo json_encode(['discount' => (float)getSetting('reseller_discount', 50)]);
